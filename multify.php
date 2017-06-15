6c86ffa61d3d6355ae9a65bf2f1e3cee4a8357f9
217LIANG <330170499@qq.com>
2017-06-15 09:40:17 +0800

    倒计时

---
+++ multify.php
@@ -0,0 +1,42 @@
+﻿<HTML>
+<?php
+//require("index.inc");
+//echo time()."<br/>";
+//$now=mktime();
+//foreach($nowdate as $new){
+	//echo $new."<br/>";
+//}
+//foreach($nowdate as $key=>$value){
+	//echo $key."=>".$value;
+	//echo "<br/>";
+//}
+//echo"今天是".$nowdate["year"]."年".$nowdate["month"]."月".$nowdate["mday"]."日";
+echo date("F j l");
+$n=$_GET["text1"];
+$m=$_GET["text2"];
+echo"<table border='1'>";
+echo"<tr>";
+echo"<td>*";
+echo"</td>";
+for($j=1;$j<=$m;$j++){
+echo"<td>";
+echo $j;
+echo"</td>";
+}
+echo"<tr/>";
+for($i=0;$i<=$n;$i++){
+    echo"<tr>";
+    echo"<td>".$i."</td>";
+for($j=1;$j<=$m;$j++){
+	echo"<td>";
+	echo $i."*".$j."=".$i*$j."\t";
+	echo"</td>";
+}
+echo"<tr/>";
+echo"<br/>";
+	
+}
+echo"</table>";
+?>
+
+</HTML>
