74cc76fabd6687f9d9d17128385b992d734e450d
217LIANG <330170499@qq.com>
2017-06-20 17:59:04 +0800

    Delete file6.php

--- file6.php
+++
@@ -1,9 +0,0 @@
-<form action="file7.php" method="post">
-<select name="number">
-　<option value="1" name="fi">1</option>
-　<option value="2" name="s">2</option>
-　<option value="3" name="t">3</option>
-　<option value="4" name="f">4</option>
-</select>
-  <input type="submit" value="submit">
-</form>
